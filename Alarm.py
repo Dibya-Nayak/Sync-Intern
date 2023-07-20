18d8a521db62f5034a106525d8360f653c25d84c
Dibyajyoti Nayak <78548442+Dibya-Nayak@users.noreply.github.com>
2023-07-20 12:16:20 +0530

    Update Alarm.py

--- Alarm.py
+++ Alarm.py
@@ -44,6 +44,6 @@ def update_time():
     time_label.config(text=current_time)
     root.after(1000, update_time)
 
-update_time()  # Start updating the time every second
+update_time()  
 
 root.mainloop()
